87efc5c99c2f4a7735874039938f22ca7fdb5871
Sebastian <seperdom@gmail.com>
2021-06-18 12:26:15 -0300

    update .env

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -17,7 +17,7 @@ class NotificationController extends Controller
 
 
 	public function notification(Request $request){
-		http_response_code(200);
+		var_dump(http_response_code(200));
 
    	 SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
 
