bcf2ebf180e9e3fbcac421339b674e901182b961
Sebastian <seperdom@gmail.com>
2021-06-18 14:03:22 -0300

    update .env

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 require __DIR__  . '/../../../vendor/autoload.php';
 
 use MercadoPago\Item;
@@ -16,28 +17,28 @@ class NotificationController extends Controller
 {
 
 
-	public function notification(Request $request){
+	public function getNotification(Request $request){
 
 
    	 SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
 
-    switch($_POST["topic"]) {
+    switch($_GET["topic"]) {
 
         case "payment":
-            $payment = Payment::find_by_id($_POST["id"]);
-            $response=json_decode($payment, true);
+            $payment = Payment::find_by_id($_GET["id"]);
+            //$response=json_encode($payment, true);
             //var_dump($response);
             console_log($response);
             break;
-        case "topic":
-            $plan = MerchantOrder::find_by_id($_POST["id"]);
-            $response=json_decode($plan, true);
-            console_log($response);
+        case "merchant_order":
+            $plan = MerchantOrder::find_by_id($_GET["id"]);
+            $response=json_encode($plan, true);
+            var_dump($response);
             break;
         case "subscription":
             $plan = Subscription::find_by_id($_POST["id"]);
             $response=json_decode($plan, true);
-            var_dump($response);
+            console_log($response);
             break;
         case "invoice":
             $plan = Invoice::find_by_id($_POST["id"]);
@@ -50,4 +51,38 @@ class NotificationController extends Controller
 
    	}
 
+
+   	public function postNotification(Request $request){
+
+
+   	 SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
+
+    switch($_POST["topic"]) {
+
+        case "payment":
+            $payment = Payment::find_by_id($_POST["id"]);
+            //$response=json_encode($payment, true);
+            //var_dump($response);
+            console_log($response);
+            break;
+        case "merchant_order":
+            $plan = MerchantOrder::find_by_id($_POST["id"]);
+            $response=json_encode($plan, true);
+            var_dump($response);
+            break;
+        case "subscription":
+            $plan = Subscription::find_by_id($_POST["id"]);
+            $response=json_decode($plan, true);
+            console_log($response);
+            break;
+        case "invoice":
+            $plan = Invoice::find_by_id($_POST["id"]);
+            $response=json_decode($plan, true);
+            var_dump($response);
+            break;
+    }
+
+    return response('OK', 200);
+
+   	}
 }
