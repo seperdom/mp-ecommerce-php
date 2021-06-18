6e3aeab512ab15390e6a5f683db8fcb4eabd97b8
Sebastian <seperdom@gmail.com>
2021-06-18 02:45:44 -0300

    update .env

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -21,10 +21,10 @@ class NotificationController extends Controller
    	 http_response_code(200);
    	 SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
 
-    switch($_POST["type"]) {
+    switch($_GET["type"]) {
 
         case "payment":
-            $payment = Payment::find_by_id($_POST["id"]);
+            $payment = Payment::find_by_id($_GET["id"]);
             $response=json_decode($payment, true);
             var_dump($response);
             break;
